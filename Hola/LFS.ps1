73c08391cb5b8d4c209a547cb446a5d29fb10341
instntKaRma <146196973+1nstntKaRma@users.noreply.github.com>
2024-04-24 18:32:05 +0200

    Update LFS.ps1

--- Hola/LFS.ps1
+++ Hola/LFS.ps1
@@ -1,3 +1,7 @@
-﻿git add "C:\Users\GolaN\Desktop\1\AIDA64Extreme.exe"
+﻿git add "C:\GolaN\- Windows + Office\SaltSafeStorage\Hola\AIDA64Extreme.exe"
+git add "C:\GolaN\- Windows + Office\SaltSafeStorage\Hola\SDIO.zip"
+git add "C:\GolaN\- Windows + Office\SaltSafeStorage\Hola\CCleanerInstall.zip"
+git add "C:\GolaN\- Windows + Office\SaltSafeStorage\Hola\Spotify.zip"
+git add "C:\GolaN\- Windows + Office\SaltSafeStorage\Hola\TreeSize.zip"
 git commit -m "LFS PowerShell"
 git push origin main
